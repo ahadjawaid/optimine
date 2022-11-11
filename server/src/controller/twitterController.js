abb7e65951285398a3686e98991a47c7bd0302a9
Bfaraira393 <farairab@gmail.com>
2022-11-11 16:20:10 -0600

    removed needle

--- server/src/controller/twitterController.js
+++ server/src/controller/twitterController.js
@@ -1,20 +1,19 @@
 // Search for Tweets within the past seven days
-const needle = require('needle');
 const config = require("../config");
+const axios = require('axios');
 
 async function getRequest(hashtag) {
-    const params = {
+    const param = {
         'query': `#${hashtag} lang:en`,
         'tweet.fields': 'public_metrics',
         'expansions': 'author_id',
         'max_results': '100'
     }
-    const res = await needle('get', 'https://api.twitter.com/2/tweets/search/recent', params, {
+    const res = await axios.get({url: 'https://api.twitter.com/2/tweets/search/recent', params: param, 
         headers: {
             "User-Agent": "v2RecentSearchJS",
             "authorization": `Bearer ${config.userfrontAPIKey}`
-        }
-    })
+        }})
     if (res.body) {
         return res.body;
     } else {
