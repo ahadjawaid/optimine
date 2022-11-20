eb33e411257a6aa83a55c883b367b07394824300
Bfaraira393 <farairab@gmail.com>
2022-11-20 14:39:28 -0600

    tweetController Update

--- server/src/controller/twitterController.js
+++ server/src/controller/twitterController.js
@@ -1,11 +1,12 @@
 // Search for Tweets within the past seven days
 const config = require("../config");
 const axios = require('axios');
+const { Analysis } = require("../models/Analysis");
 
-async function getRequest(hashtag) {
+async function getRequest(hashtag,requestUuid) {
     const param = {
         'query': `#${hashtag} lang:en`,
-        'tweet.fields': 'public_metrics',
+        'tweet.fields': ["created_at","id","public_metrics"],
         'expansions': 'author_id',
         'max_results': '100'
     }
@@ -15,10 +16,26 @@ async function getRequest(hashtag) {
             "authorization": `Bearer ${config.userfrontAPIKey}`
         }})
     if (res.body) {
-        return res.body;
-    } else {
         throw new Error('Unsuccessful request');
     }
+    let tweetData = res.data;
+    let tweetUsers = res.users;
+    let tweetMeta = res.meta;
+    for (let i = 0; i < tweetMeta.result_count; i++) {
+        let tweet = {
+            body: tweetData[i].text,
+            poster: tweetUsers[i].username,
+            date: tweetData[i].created_at,
+            retweets: tweetData[i].public_metrics.retweet_count,
+            quoteTweets: tweetData[i].public_metrics.quote_count,
+            replies: tweetData[i].public_metrics.reply_count,
+            likes: tweetData[i].public_metrics.like_count, 
+            sentiment: 1
+        }
+        let doc = await Student.findOne({ uuid: requestUuid })
+        doc.tweets.push(tweet)
+        await doc.save()
+    };
 }
 (async () => {
     try {
